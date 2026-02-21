dbac369f17ed6d5520ad5796ebdee08d2343115f
reyk-create <reyanshkini@gmail.com>
2026-02-21 11:46:05 -0800

    Create python-cheat-sheet.py

---
+++ python-cheat-sheet.py
@@ -0,0 +1,53 @@
+# Python Cheatsheet
+
+# Print Functions
+
+# What Are They
+# It prints what you want it to print 
+
+# Example
+# print("Reyansh")
+
+# Variables
+
+# They are letters which you give value to
+
+# x = 1
+# print(x) 
+
+# If Statements
+
+# An if statement checks if you have that item
+
+# if x == 1:
+#  print(x)
+# Elif Statements
+
+# A elif statement checks something to see if its true
+
+# a = 45
+# b = 50
+# if a > b:
+#  print("a is greater than b")
+# elif b > a:
+#  print("b is actually greater than a") 
+# Else Statements
+
+# an else statement just runs when everything before it is false
+
+# a = 45
+# b = 50
+# if a > b:
+#  print("a is greater than b")
+# elif a == b:
+#  print(a is equal to b")
+# else:
+#  print("b is greater than a")
+
+# For Loops
+
+# It loops through your list to see if its there or not
+
+# grocery = ("apples","banana", "Melon")
+# for i in grocery:
+#   print(i) 
