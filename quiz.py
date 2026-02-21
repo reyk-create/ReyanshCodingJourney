11b0eb0fa38ad8e6b8b15aca96a89c100a6ae4c3
reyk-create <reyanshkini@gmail.com>
2026-02-21 12:05:00 -0800

    Create quiz.py

---
+++ quiz.py
@@ -0,0 +1,13 @@
+input("Welcome to the Quiz, What is your name: ")
+x = input("Would you like to take the Quiz, Yes or No: ")
+if x == "Yes":
+  print("Let's move on")
+elif x == "No":
+  print("No Quiz for you")
+else:
+  print("Try again")
+y = input(" What is 1+1: ")
+if y == "2":
+  print("You got it")
+else:
+  print("Its wrong")
